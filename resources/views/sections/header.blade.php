a79e0430236c3f99084c57ff4363004e06a4453e
DevHouse <service@devhouse.se>
2023-05-05 14:38:56 +0000

    General styling for header section and set up of scss directories.

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,4 +1,4 @@
-<header class="banner flex p-4">
+<header class="banner flex justify-between items-center p-4">
   <a class="brand w-[100px]" href="{{ home_url('/') }}">
     {{--{!! $siteName !!}--}}
     <img class="logo" src="/wp-content/themes/devhouse-sage-starter/resources/images/DHlogo-black.png" alt="DevHouse logo"/>
@@ -6,7 +6,7 @@
 
   @if (has_nav_menu('primary_navigation'))
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex justify-between', 'echo' => false]) !!}
     </nav>
   @endif
 </header>
