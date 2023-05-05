0a159d2a36ba4784643976754e9c861c8574dfc0
DevHouse <service@devhouse.se>
2023-05-05 08:07:04 +0000

    Add existing project files to Git

---
+++ resources/views/blocks/demo-block.blade.php
@@ -0,0 +1,22 @@
+{{--
+  Title: Demo Block for Ross & Alek
+  Description: Customer testimonial
+  Category: formatting
+  Icon: admin-comments
+  Keywords: testimonial quote
+  Mode: edit
+--}}
+
+<blockquote data-{{ $block['id'] }} class="{{ $block['classes'] }}">
+    <p>{{ get_field('testimonial') }}</p>
+    <cite>
+      <span>{{ get_field('author') }}</span>
+    </cite>
+</blockquote>
+
+<style type="text/css">
+  [data-{{$block['id']}}] {
+    background: {{ get_field('background_color') }};
+    color: {{ get_field('text_color') }};
+  }
+</style>
